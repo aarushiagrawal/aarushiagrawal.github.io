142e22750d5dd96d7c648cc37c85447c183f402e
SID AG <agsid@gmail.com>
2026-03-03 12:57:42 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   output: 'static',
-  plugins: [
-    tailwindcss(),
-  ],
 });
